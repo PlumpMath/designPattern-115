d5c752ad9795e58c0ca84b98c7164866de65be1f
lzh984294471 <984294471@qq.com>
2016-06-24 14:57:37 +0800

    修改git.sh

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@ if [ ! -n $1 ];then
 	echo "\033[31m please input your branch !!!! \033[0m"
 	exit
 elif [ ! -n $2 ];then
-	echo ""\033[31m please input your commit message !!!! \033[0m""
+	echo "\033[31m please input your commit message !!!! \033[0m"
 else
 	git add -A && git commit -m $2 && git pull origin $1 && git push origin $1
 fi
