e78deab658075e15423b72753875bda71162bdf8
lzh984294471 <984294471@qq.com>
2016-07-02 08:27:17 +0800

    修改：     shells/git.sh

--- shells/git.sh
+++ shells/git.sh
@@ -1,9 +1,13 @@
 #/bin/bash
-if [ ! -n $1 ];then
+if test -z $1 ;then
 	echo "\033[31m please input your branch !!!! \033[0m"
-	exit
-elif [ ! -n $2 ];then
-	echo "\033[31m please input your commit message !!!! \033[0m"
+	exit 1
 else
-	git add -A && git commit -m $2 && git pull origin $1 && git push origin $1
+	if  test -z $2 ;then 
+		git add -A && git commit  && git pull origin $1 && git push origin $1
+		exit 0
+	else
+		git add -A && git commit -m $2  && git pull origin $1 && git push origin $1
+	fi
+	exit 0
 fi
