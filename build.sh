c770253b7a9356507f4585373485fdbaeb333efc
lzh984294471 <984294471@qq.com>
2016-07-09 20:01:50 +0800

    修改：     build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,8 @@
 # author: lzh
 curr=`pwd`
 echo 'scaning '
-find -name "*.java" > "source.txt"
+file=`find -name "*.java"`
 echo "scan finish"
-javasource=`cat source.txt`
 echo 'compiling'
-javac -source 1.8 -target 1.8  -d out $javasource -Xlint:unchecked
+javac -source 1.8 -target 1.8  -d out $file -Xlint:unchecked
 echo 'compile  finish'
-echo 'remove source.txt'
-rm source.txt
